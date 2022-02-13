07388a23094852051f18c100978eb523fa0c5e4f
someonewholikesomecode <ego2004riy@gmail.com>
2022-02-14 03:38:08 +0500

    no callbacks

--- utils.js
+++ utils.js
@@ -1,8 +1,8 @@
 const {http,request} = require('http');
 
 function sendLines(msg1, msg2) {
-	const reqFirstLine = request(`http://192.168.1.140:3000/?text=[${msg1}]&line=0`, _ => { });
-	const reqSecondLine = request(`http://192.168.1.140:3000/?text=${msg2}&line=1`, _ => { });
+	const reqFirstLine = request(`http://192.168.1.140:3000/?text=[${msg1}]&line=0`);
+	const reqSecondLine = request(`http://192.168.1.140:3000/?text=${msg2}&line=1`);
 	reqFirstLine.on('error', e => console.error(`problem with ${e.message}`));
 	reqSecondLine.on('error', e => console.error(`problem with ${e.message}`));
 	reqFirstLine.end();
